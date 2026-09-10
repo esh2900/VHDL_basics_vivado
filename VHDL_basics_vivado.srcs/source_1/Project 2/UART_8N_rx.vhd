904324543b16e70a5896c0529d408cf788e53633
esh2900 <brunogiulianism@gmail.com>
2026-09-10 14:11:17 +0200

    Implemented the FULL RTL of my UART RX.
    Now I will start to implement the testbenches for the system.

--- VHDL_basics_vivado.srcs/source_1/Project 2/UART_8N_rx.vhd
+++ VHDL_basics_vivado.srcs/source_1/Project 2/UART_8N_rx.vhd
@@ -16,43 +16,100 @@ entity UART_8N_rx is
 
          message_out : out std_logic_vector(7 downto 0);
          data_valid: out std_logic;
-         framing_error : out std_logic;
-
+         framing_error : out std_logic
     );
 
 end entity;
 
-
-
 architecture rtl of UART_8N_rx is
 
-    signal baud_reset : std_logic :='0';
-    signal baud_tick : std_logic;
+    type state_type is ( -- FSM States
+    IDLE,
+    START_CHECK,
+    DATA_STREAM,
+    STOP_CHECK
+    );
+    
+    -- State Recognition Variables
+    signal state : state_type := IDLE;
+    signal bit_index : integer range 0 to 8 := 0; -- Checar esse limite
+    
+    -- RX Signal Syncronizer
+    signal rx_meta : std_logic := '1';
+    signal rx_sync : std_logic := '1';
+
+    constant CLKS_PER_BIT : positive := CLK_FREQ / BAUD_RATE;
+    signal counter : integer range 0 to CLKS_PER_BIT := 0;    
 
 
 begin
 
-    baud_generator_signal : entity work.baud_generator
-        generic map(
-            CLK_FREQ => CLK_FREQ,
-            BAUD_RATE => BAUD_RATE
-        ),
-        port map(
-            clk => clk,
-            reset => baud_reset,
-            baud_tick => baud_tick
-        );
-
-    
         process(clk)
         begin
-            
             if rising_edge(clk) then
+                data_valid <= '0';
+                framing_error <='0';
+                
+                -- rx_sync reduces de risk of metastability on the data, at the cost
+                -- of a 2 clock pulses delay, this is useful because the message may come
+                -- from a different clock domain in another device and cause instability.
+                rx_meta <= rx;
+                rx_sync <= rx_meta;
 
+                if (state /= IDLE) then
+                    counter <= counter+1;
+                end if;
 
+                if (reset='1') then
+                    state <= IDLE;
+                    bit_index <=0;
+                    rx_meta <='1';
+                    rx_sync <='1';
+                    counter <= 0;
+                    message_out <= (others => '0');
+                else
+                    case state is
+                        when IDLE =>
+                            if (rx_sync='0') then
+                                state <= START_CHECK;
+                            end if;
+                        
+                        when START_CHECK =>
+                            if (rx_sync='0' and counter >= CLKS_PER_BIT/2) then 
+                                state <= DATA_STREAM; -- Message Arived
+                                counter <=0;
+                            elsif(counter >= CLKS_PER_BIT/2) then
+                                state <= IDLE; -- False Start
+                                counter <=0;
+                            end if;
+
+                        when DATA_STREAM =>
+                            if(counter = CLKS_PER_BIT-1) then
+                                message_out(bit_index) <= rx_sync;
+                                counter <= 0;
+
+                                if(bit_index=7) then -- Final bit transmitted.
+                                bit_index<=0;
+                                state <= STOP_CHECK;
+                                else
+                                    bit_index<=bit_index+1;
+                                end if;
+                            end if;
+
+                        when STOP_CHECK =>
+                            if(counter = CLKS_PER_BIT-1) then
+                                counter <= 0;
+                                state <= IDLE;
+                                if (rx_sync='1') then -- Message Transmisison Sucessfull
+                                    data_valid <= '1';
+                                else
+                                    framing_error <='1';
+                                end if;
+                            end if;
+                    end case;
+                end if;
 
             end if;
 
         end process;
-
 end architecture;
